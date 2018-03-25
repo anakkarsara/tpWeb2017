3d089c6acfa23cc7d25633f1d04c805a898ee731
Kezibri Anas <anas.kezibri@gmail.com>
2018-03-25 14:44:18 +0200

    Tests on Main

--- interaction.js
+++ interaction.js
@@ -1,20 +1,56 @@
-
-// La création d'un Dnd requière un canvas et un interacteur.
-// L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+  this.initX = 0;
+  this.initY = 0;
+  this.finalX = 0;
+  this.finalY = 0;
+  this.pression = false;
+  this.moving = false;
 
-	// Developper les 3 fonctions gérant les événements
 
-	// Associer les fonctions précédentes aux évènements du canvas.
-};
+  this.mousedown = function (evt) {
+    var pos = getMousePosition(canvas, evt);
+    this.initX = pos.x;
+    this.initY = pos.y;
+    this.pression = true;
+    interactor.onInteractionStart(this);
 
+    console.log("mousedown");
+  }.bind(this);
 
+  this.mousemove = function (evt) {
+    if (this.pression){
+      this.moving = true;
+      var pos = getMousePosition(canvas, evt);
+      this.finalX = pos.x;
+      this.finalY = pos.y;
+      interactor.onInteractionUpdate(this);
+      console.log("mousemove");
+    }
+  }.bind(this);
+
+  this.mouseup = function (evt) {
+    if (this.moving){
+      var pos = getMousePosition(canvas, evt);
+      this.finalX = pos.x;
+      this.finalY = pos.y;
+      this.pression = false;
+      this.moving = false;
+
+      interactor.onInteractionEnd(this);
+      console.log("mouseup");
+    }
+  }.bind(this);
+
+  // Associer les fonctions précédentes aux évènements du canvas.
+  canvas.addEventListener('mousedown', this.mousedown, false);
+  canvas.addEventListener('mousemove', this.mousemove, false);
+  canvas.addEventListener('mouseup', this.mouseup, false);
+}
 // Place le point de l'événement evt relativement à la position du canvas.
 function getMousePosition(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
   return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top
+    x: Math.round(evt.clientX - rect.left),
+    y: Math.round(evt.clientY - rect.top)
   };
-};
+}
--- main.js
+++ main.js
@@ -1,26 +1,58 @@
-
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
-canvas.width=800
-canvas.height=600
-
-// Code temporaire pour tester le DnD
-new DnD(canvas);
-ctx.fillStyle = '#F0F0F0'; // set canvas' background color
-ctx.fillRect(0, 0, canvas.width, canvas.height);  // now fill the canvas
-/////
+canvas.width = 700;
+canvas.height = 600;
 
 // Code temporaire pour tester l'affiche de la vue
-var rec = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
-rec.paint(ctx);
-var ligne = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
-ligne.paint(ctx);
-// tester également Dessin.
-////
+var rec = new Rectangle(10, 20, 200, 100, 10, 'red',0);
+var ligne = new Line(20, 500, 275, 133, 8, '#000AAA');
+var ligne2 = new Line(660, 150, 422, 470, 10, 'green',0);
+var circle = new Circle(350, 200, 100, 10, 'blue',1);
+var circle2 = new Circle(350, 400, 100, 10, 'red',0);
 
-// Code final à utiliser pour manipuler Pencil.
 var drawing = new Drawing();
-var pencil = new Pencil(ctx, drawing, canvas);
+
+drawing.undoRedo.ajouter(rec);
+drawing.undoRedo.ajouter(ligne);
+drawing.undoRedo.ajouter(ligne2);
+drawing.undoRedo.ajouter(circle);
+drawing.undoRedo.ajouter(circle2);
+
 drawing.paint(ctx, canvas);
 
+// Code final à utiliser pour manipuler Pencil.
+var pencil = new Pencil(ctx, drawing, canvas);
+
+function removeForm(index) {
+  drawing.undoRedo.supprimer(index);
+  pencil.updateShapeList();
+  drawing.paint(ctx, canvas);
+  console.log("remove : " + index);
+}
+
+function switchShape(index) {
+  switch (index) {
+    case editingMode.rect:
+      pencil.currEditingMode = index;
+      break;
+    case editingMode.line:
+      pencil.currEditingMode = index;
+      break;
+    case editingMode.circle:
+      pencil.currEditingMode = index;
+      break;
+  }
+}
+
+function undo() {
+  drawing.undoRedo.undo();
+  drawing.paint(ctx, canvas);
+  pencil.updateShapeList();
+}
+
+function redo() {
+  drawing.undoRedo.redo();
+  drawing.paint(ctx, canvas);
+  pencil.updateShapeList();
+}
